ae1ce513c1f8b5e7c930ceee0254be6f4bc6ab30
szentedaniel <karifull2000@gmail.com>
2022-03-22 20:51:11 +0100

    felesleges cuccok törlése

--- server/functions.js
+++
@@ -1,9 +0,0 @@
-const generateRoomCode = (list) => {
-  let code = null
-  do {
-    code = Math.floor(Math.random() * 900000 + 100000)
-  } while (list.has(code.toString()))
-  return code.toString()
-}
-
-module.exports = generateRoomCode
