b6093ba0505034388ab4637db99a4768840a00d7
szentedaniel <karifull2000@gmail.com>
2022-05-03 00:27:35 +0200

    fixed bug that makes instatntly win the game on the start

--- client/src/Components/GameComponents/Winner.jsx
+++ client/src/Components/GameComponents/Winner.jsx
@@ -1,77 +1,81 @@
 import { Modal } from '@mantine/core'
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import pinkAvatar from '../../Images/game/moreenemiesanimations/Alien sprites/alienPink.png'
 import blueAvatar from '../../Images/game/moreenemiesanimations/Alien sprites/alienBlue.png'
 import yellowAvatar from '../../Images/game/moreenemiesanimations/Alien sprites/alienYellow.png'
 import greenAvatar from '../../Images/game/moreenemiesanimations/Alien sprites/alienGreen.png'
 import { useNavigate } from 'react-router-dom'
 import { useSocket } from '../../Contexts/SocketContext'
+import { resetState } from '../../Store/slices/gameStateSlice'
 
 export default function Winner() {
-    const { gameOver, winnerColor } = useSelector((state) => state.gameState)
-    const navigate = useNavigate()
-    const socket = useSocket()
+  const { gameOver, winnerColor } = useSelector((state) => state.gameState)
+  const navigate = useNavigate()
+  const socket = useSocket()
+  const dispatch = useDispatch()
 
-    const [opened, setOpened] = useState(false)
+  const [opened, setOpened] = useState(false)
 
-    let image = pinkAvatar
-    if (winnerColor === 1) {
-        image = blueAvatar
-    } else if (winnerColor === 2) {
-        image = greenAvatar
-    } else if (winnerColor === 2) {
-        image = yellowAvatar
-    }
+  let image = pinkAvatar
+  if (winnerColor === 1) {
+    image = blueAvatar
+  } else if (winnerColor === 2) {
+    image = greenAvatar
+  } else if (winnerColor === 2) {
+    image = yellowAvatar
+  }
 
-    useEffect(() => {
-        if (gameOver) {
-            setOpened(true)
-        } else { setOpened(false) }
-    }, [gameOver])
+  useEffect(() => {
+    if (gameOver) {
+      setOpened(true)
+    } else { setOpened(false) }
+  }, [gameOver])
 
-    const onColseHandler = () => {
-        setOpened(false)
-        navigate('/')
-        socket.emit('leave_room')
-    }
+  const onColseHandler = () => {
+    setOpened(false)
+    navigate('/')
+    dispatch(resetState())
+    socket.emit('leave_room')
 
-    return (
-        <>
-            <Modal
-                size="lg"
-                // size="calc(100vw - 20vw)"
-                centered
-                opened={opened}
-                onClose={onColseHandler}
-                title={'győzelem'.toUpperCase()}
-                transition='slide-up'
-                transitionDuration={300}
-                transitiontimingfunction='ease'
-                closeOnClickOutside={false}
-                closeOnEscape={false}
-                withCloseButton={false}
-                radius={'lg'}
-                classNames={{ modal: 'bg-[#F5ECE3] text-center' }}
-            //overlayColor={'#F5ECE3'}
-            >
-                <div className='flex flex-col justify-center items-center w-auto'>
-                    <div className='flex flex-col items-center content-center w-32 h-32 rounded m-3 drop-shadow-md'>
-                        <img src={image} alt="winner" />
-                    </div>
+  }
 
-                    <div className='flex flex-col w-4/5 h-32 rounded-lg bg-[#eedac6] self-center items-center justify-evenly mb-3 p-3 leading-5'>
-                        <span>Gratulálunk!!!</span>
-                        <p><span></span></p>
-                    </div>
+  return (
+    <>
+      <Modal
+        size="lg"
+        // size="calc(100vw - 20vw)"
+        centered
+        opened={opened}
+        onClose={onColseHandler}
+        title={'győzelem'.toUpperCase()}
+        transition='slide-up'
+        transitionDuration={300}
+        transitiontimingfunction='ease'
+        closeOnClickOutside={false}
+        closeOnEscape={false}
+        withCloseButton={false}
+        radius={'lg'}
+        classNames={{ modal: 'bg-[#F5ECE3] text-center' }}
+      //overlayColor={'#F5ECE3'}
+      >
+        <div className='flex flex-col justify-center items-center w-auto'>
+          <div className='flex flex-col items-center content-center w-32 h-32 rounded m-3 drop-shadow-md'>
+            <img src={image} alt="winner" />
+          </div>
 
-                    <div
-                        onClick={onColseHandler}
-                        className='flex w-fit h-fit px-4 py-2 bg-orange-300 border-orange-100 border-4 cursor-pointer rounded-full items-center justify-center hover:bg-orange-400 shadow-lg'>Csodás</div>
-                </div>
+          <div className='flex flex-col w-4/5 h-32 rounded-lg bg-[#eedac6] self-center items-center justify-evenly mb-3 p-3 leading-5'>
+            <span>Gratulálunk!!!</span>
+            <p><span></span></p>
+          </div>
 
-            </Modal>
-        </>
+          <div
+            onClick={onColseHandler}
+            className='flex w-fit h-fit px-4 py-2 bg-orange-300 border-orange-100 border-4 cursor-pointer rounded-full items-center justify-center hover:bg-orange-400 shadow-lg'>Csodás</div>
+        </div>
 
-    )
+      </Modal>
+    </>
+
+  )
 }
--- client/src/Store/slices/gameStateSlice.js
+++ client/src/Store/slices/gameStateSlice.js
@@ -6,6 +6,327 @@ import { join } from 'lodash'
 
 const end = new Date(new Date().getTime() + 50 * 60000).toISOString()
 
+const initState = {
+  roomCode: null,
+  isLoaded: false,
+  currentPlayer: 0,
+  showDiceRoll: true,
+  showBalance: false,
+  showBuyPanel: false,
+  showFirstQuarantineTab: false,
+  showQuarantineTab: false,
+  rollingDiceFromQuarantine: false,
+  showDoubler: false,
+  selectDoubler: false,
+  showErasmus: false,
+  selectErasmus: false,
+  showDisabledFields: false,
+  singleSelecting: false,
+  multipleSelecting: false,
+  onlyOwnField: false,
+  selectedFields: [],
+  showSell: false,
+  showTax: false,
+  sellValue: 0,
+  sellIsFrom: '',
+  lastDiceRoll: 0,
+  museumPrice: 400_000,
+  roundBonus: 250_000,
+  resetCountdown: 1,
+  currentPlayerIndex: 0,
+  gameOver: false,
+  winnerColor: null,
+  endDate: end,
+  selectedCard: 0,
+  showCard: false,
+  players: [
+    {
+      username: 'user' + Math.floor(Math.random() * 999999),
+      userId: null,
+      colorCode: 0,
+      money: 2_500_000,
+      hasPCR: false,
+      coutPCR: 0,
+      isInQuarantine: false,
+      QuarantineRounds: 0,
+      field: 0,
+      timeRemain: 0,
+      playerCountdown: true,
+      isBankrupt: false
+
+    },
+    {
+      username: 'user' + Math.floor(Math.random() * 999999),
+      userId: null,
+      colorCode: 1,
+      money: 2_500_000,
+      hasPCR: false,
+      coutPCR: 0,
+      isInQuarantine: false,
+      QuarantineRounds: 0,
+      field: 0,
+      timeRemain: 0,
+      playerCountdown: false,
+      isBankrupt: false
+    },
+    {
+      username: 'user' + Math.floor(Math.random() * 999999),
+      userId: null,
+      colorCode: 2,
+      money: 2_500_000,
+      hasPCR: false,
+      coutPCR: 0,
+      isInQuarantine: false,
+      QuarantineRounds: 0,
+      field: 0,
+      timeRemain: 0,
+      playerCountdown: false,
+      isBankrupt: false
+    },
+    {
+      username: 'user' + Math.floor(Math.random() * 999999),
+      userId: null,
+      colorCode: 3,
+      money: 2_500_000,
+      hasPCR: false,
+      coutPCR: 0,
+      isInQuarantine: false,
+      QuarantineRounds: 0,
+      field: 0,
+      timeRemain: 0,
+      playerCountdown: false,
+      isBankrupt: false
+    },
+
+  ],
+  map: [
+    {
+      id: 0,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 1,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 2,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 3,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 4,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 5,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 6,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 7,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 8,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 9,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 10,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 11,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 12,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 13,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 14,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 15,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 16,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 17,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 18,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 19,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 20,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 21,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 22,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 23,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 24,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 25,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 26,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 27,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 28,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 29,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 30,
+      level: null,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+    {
+      id: 31,
+      level: 0,
+      ownerColor: null,
+      hasDoubler: false,
+      doublerCount: 0
+    },
+  ],
+}
+
 export const gameStateSlice = createSlice({
   name: 'gameState',
   initialState: {
@@ -964,6 +1285,9 @@ export const gameStateSlice = createSlice({
     },
     setSelectedCard: (state, action) => {
       state.selectedCard = action.payload
+    },
+    resetState: (state, action) => {
+      state = initState
     }
 
 
@@ -1007,7 +1331,8 @@ export const {
   setShowCard,
   applyCardEffect,
   setGameState,
-  setSelectedCard
+  setSelectedCard,
+  resetState
 
 } = gameStateSlice.actions
 
